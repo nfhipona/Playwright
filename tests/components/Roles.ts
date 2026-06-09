d110d5a7b40943709bf1ee2d3543f7c177d013ad
Neil Francis Ramirez Hipona <nfhipona@users.noreply.github.com>
2026-06-09 20:21:10 +0800

    Update role component and add unit tests

--- tests/components/Roles.ts
+++ tests/components/Roles.ts
@@ -5,6 +5,27 @@ class RolesPage extends Base {
     getButton(label: string): Locator {
         return this.page.getByRole('button', { name: label });
     }
+
+    getInput(label: string): Locator {
+        return this.page.getByRole('textbox', { name: label });
+    }
+
+    getCheckbox(label: string): Locator {
+        return this.page.getByRole('checkbox', { name: label });
+    }
+
+    getNavigation(): Locator {
+        return this.page.getByRole('navigation').first();
+    }
+
+    getLists(): Locator {
+        const navigation = this.getNavigation();
+        return navigation.getByRole('menuitem');
+    }
+
+    getAlert(): Locator {
+        return this.page.getByRole('alert');
+    }
 }
 
 export default RolesPage;
--- tests/phase1/roles.spec.ts
+++ tests/phase1/roles.spec.ts
@@ -15,4 +15,40 @@ test.describe('Roles', () => {
         const div = rolesPage.getButton('Div with button role');
         await expect(div).toBeVisible();
     });
+
+    test('should find form elements by role', async ({ page }) => {
+        const rolesPage = new RolesPage(page);
+        await rolesPage.load();
+
+        const input = rolesPage.getInput('username');
+        await input.fill('my_username_text');
+        await expect(input).toHaveValue('my_username_text');
+
+        const checkbox = rolesPage.getCheckbox('Accept terms');
+        await checkbox.check();
+        await expect(checkbox).toBeChecked();
+    });
+
+    test('should find navigation elements by role', async ({ page }) => {
+        const rolesPage = new RolesPage(page);
+        await rolesPage.load();
+
+        const links = rolesPage.getNavigation();
+        await expect(links).toBeVisible();
+
+        const listItems = rolesPage.getLists();
+        await expect(listItems).toHaveCount(3);
+
+        const homeLink = listItems.nth(0);
+        await expect(homeLink).toHaveText('Home');
+
+        const productsLink = listItems.nth(1);
+        await expect(productsLink).toHaveText('Products');
+
+        const contactLink = listItems.nth(2);
+        await expect(contactLink).toHaveText('Contact');
+
+        const alert = rolesPage.getAlert();
+        await expect(alert).toHaveText(/this is an important alert message!/i);
+    });
 });
