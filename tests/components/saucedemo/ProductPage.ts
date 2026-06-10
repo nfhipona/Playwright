233316f7305ba0aefe9a68b94b524ef28314fb0e
Neil Francis Ramirez Hipona <nfhipona@users.noreply.github.com>
2026-06-10 21:51:54 +0800

    Create ProductPage model

---
+++ tests/components/saucedemo/ProductPage.ts
@@ -0,0 +1,42 @@
+import { Locator } from '@playwright/test';
+import LoginPage from "./LoginPage";
+import CartPage from './CartPage';
+
+class ProductPage extends LoginPage {
+    protected readonly productURL: string = `${this.baseURL}/inventory.html`; // https://www.saucedemo.com/inventory.html
+
+    async getProductLists(): Promise<Locator> {
+        return this.locateBy('.inventory_list');
+    }
+
+    async getProductItems(): Promise<Locator> {
+        return this.locateBy('.inventory_item');
+    }
+
+    async addToCartByName(productName: string): Promise<void> {
+        const items = await this.getProductItems();
+        const addToCartButton = items.filter({ hasText: productName }).locator('button', { hasText: 'Add to cart' });
+        await addToCartButton.click();
+    }
+
+    async removeFromCartByName(productName: string): Promise<void> {
+        const items = await this.getProductItems();
+        const removeFromCartButton = items.filter({ hasText: productName }).locator('button', { hasText: 'Remove' });
+        await removeFromCartButton.click();
+    }
+
+    async getCartButton(): Promise<Locator> {
+        return this.locateBy('.shopping_cart_link');
+    }
+
+    async getSortDropdown(): Promise<Locator> {
+        return this.locateBy('.product_sort_container');
+    }
+
+    async sortProductsBy(option: string): Promise<void> {
+        const sortDropdown = await this.getSortDropdown();
+        await sortDropdown.selectOption(option);
+    }
+}
+
+export default ProductPage;
