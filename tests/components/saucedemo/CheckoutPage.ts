531c952584cea1b9678389c2307d0bd633273d7e
Neil Francis Ramirez Hipona <nfhipona@users.noreply.github.com>
2026-06-15 20:43:29 +0800

    Update checkout page model

--- tests/components/saucedemo/CheckoutPage.ts
+++ tests/components/saucedemo/CheckoutPage.ts
@@ -2,17 +2,29 @@ import { Locator } from '@playwright/test';
 import CartPage from "./CartPage";
 
 class CheckoutPage extends CartPage {
-    protected readonly checkoutStepOneURL: string = `${this.baseURL}/checkout-step-one.html`; // https://www.saucedemo.com/checkout-step-one.html
-    protected readonly checkoutStepTwoURL: string = `${this.baseURL}/checkout-step-two.html`; // https://www.saucedemo.com/checkout-step-two.html
+    protected readonly checkoutStepTwoURLPattern: RegExp = /.*checkout-step-two.html/;
+    protected readonly cartURLPattern: RegExp = /.*cart.html/;
 
     async getCheckoutTitle(): Promise<Locator> {
         return this.locateBy('.title');
     }
 
+    async firstNameInput(): Promise<Locator> {
+        return this.locateBy('#first-name');
+    }
+
+    async lastNameInput(): Promise<Locator> {
+        return this.locateBy('#last-name');
+    }
+
+    async postalCodeInput(): Promise<Locator> {
+        return this.locateBy('#postal-code');
+    }
+
     async fillCheckoutInformation(firstName: string, lastName: string, postalCode: string): Promise<void> {
-        const firstNameInput = this.page.locator('#first-name');
-        const lastNameInput = this.page.locator('#last-name');
-        const postalCodeInput = this.page.locator('#postal-code');
+        const firstNameInput = await this.firstNameInput();
+        const lastNameInput = await this.lastNameInput();
+        const postalCodeInput = await this.postalCodeInput();
 
         await firstNameInput.fill(firstName);
         await lastNameInput.fill(lastName);
@@ -22,13 +34,13 @@ class CheckoutPage extends CartPage {
     async clickContinueButton(): Promise<void> {
         const continueButton = this.page.getByRole('button', { name: 'Continue' });
         await continueButton.click();
-        await this.waitForURL(/.*checkout-step-two.html/);
+        await this.waitForURL(this.checkoutStepTwoURLPattern);
     }
 
     async clickCancelButton(): Promise<void> {
         const cancelButton = this.page.getByRole('button', { name: 'Cancel' });
         await cancelButton.click();
-        await this.waitForURL(/.*cart.html/);
+        await this.waitForURL(this.cartURLPattern);
     }
 }
 
