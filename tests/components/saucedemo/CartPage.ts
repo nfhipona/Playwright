4161d595d488a168d3f5ddcb93cefefcf6dec61d
Neil Francis Ramirez Hipona <nfhipona@users.noreply.github.com>
2026-06-15 17:23:55 +0800

    Update cart page model

--- tests/components/saucedemo/CartPage.ts
+++ tests/components/saucedemo/CartPage.ts
@@ -6,22 +6,24 @@ class CartPage extends LoginPage {
     protected readonly productURL: string = `${this.baseURL}/inventory.html`; // https://www.saucedemo.com/inventory.html
     protected readonly checkoutURL: string = `${this.baseURL}/checkout-step-one.html`; // https://www.saucedemo.com/checkout-step-one.html
 
+    async getProductItems(): Promise<Locator> {
+        return this.locateBy('.inventory_item');
+    }
+
+    async addToCartByName(productName: string): Promise<void> {
+        const items = await this.getProductItems();
+        const addToCartButton = items.filter({ hasText: productName }).locator('button', { hasText: 'Add to cart' });
+        await addToCartButton.click();
+    }
+
+    async getCartButton(): Promise<Locator> {
+        return this.locateBy('.shopping_cart_link');
+    }
+
     async navigateToCart(): Promise<void> {
-        const cartButton = await this.locateBy('.shopping_cart_link');
+        const cartButton = await this.getCartButton();
         await cartButton.click();
         await this.waitForURL(this.cartURL);
-    }
-
-    async navigateToProduct(): Promise<void> {
-        const productButton = await this.locateBy('#continue-shopping');
-        await productButton.click();
-        await this.waitForURL(this.productURL);
-    }
-
-    async navigateToCheckout(): Promise<void> {
-        const checkoutButton = this.page.getByRole('button', { name: 'Checkout' });
-        await checkoutButton.click();
-        await this.waitForURL(this.checkoutURL);
     }
 
     async getCartLists(): Promise<Locator> {
@@ -30,6 +32,31 @@ class CartPage extends LoginPage {
 
     async getCartItems(): Promise<Locator> {
         return this.locateBy('.cart_item');
+    }
+
+    async clickProductByName(productName: string): Promise<void> {
+        const items = await this.getCartItems();
+        const productLink = items.filter({ hasText: productName }).locator('.inventory_item_name');
+        await productLink.click();
+        await this.waitForURL(/.*inventory-item.html\?id=\d+.*/);
+    }
+
+    async removeItemByName(productName: string): Promise<void> {
+        const items = await this.getCartItems();
+        const removeButton = items.filter({ hasText: productName }).locator('button', { hasText: 'Remove' });
+        await removeButton.click();
+    }
+
+    async backToProductPage(): Promise<void> {
+        const productButton = await this.locateBy('#continue-shopping');
+        await productButton.click();
+        await this.waitForURL(this.productURL);
+    }
+
+    async goToCheckoutPage(): Promise<void> {
+        const checkoutButton = this.page.getByRole('button', { name: 'Checkout' });
+        await checkoutButton.click();
+        await this.waitForURL(this.checkoutURL);
     }
 }
 
